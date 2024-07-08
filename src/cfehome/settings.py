9c5334ce8ead91d6c617b949d2d5bce67c039f74
AlexeyG333 <alexeyg333@yahoo.com>
2024-07-07 22:07:38 -0600

    Course V1.4

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dm8#nbkiua^ws=qqb#ra!d3jmw*c%te(#!#%078-4$hu31bv^4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") or True
+DEBUG = str(os.environ.get("DEBUG_DEBUG")).lower() == "true"
 
 print("DEBUG", DEBUG, type(DEBUG))
 
